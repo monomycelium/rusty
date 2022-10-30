1e90af8075da8797182f0f51e65ba3e597f67091
mashrafi rahman <monomycelium@icloud.com>
2022-10-30 15:59:02 +0800

    start learning about enums.

---
+++ enums/src/main.rs
@@ -0,0 +1,29 @@
+/*
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+fn main() {
+    let four = IpAddrKind::V4(127, 0, 0, 1);
+    let six = IpAddrKind::V6(String::from("::1"));
+} */
+
+/*
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    Color(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("hi.");
+    }
+}
+
+fn main() {
+    let m = Message::Write(String::from("hayes"));
+    m.call();
+} */
