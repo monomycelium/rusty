549d01a2b754d61cca8c2c0105f13f1b389ed90a
mashrafi rahman <monomycelium@icloud.com>
2022-11-21 16:05:25 +0800

    finish learning traits and generics.

--- generics/traits/src/main.rs
+++ generics/traits/src/main.rs
@@ -1,8 +1,40 @@
 pub trait Summary {
+    fn summarise_author(&self) -> String;
+
     fn summarise(&self) -> String {
-        String::from("read more...")
+        format!("read more from {}...", self.summarise_author())
     }
 }
+
+pub fn notify(item: &impl Summary) {
+    println!("breaking news! {}", item.summarise());
+}
+
+fn return_summarisable() -> impl Summary {
+    Tweet {
+        username: String::from("ayd3n"),
+        content: String::from("sunny, isn't it?"),
+        reply: false,
+        retweet: false,
+    }
+} 
+
+/*
+pub fn notify<T: Summary>(item: &T) {
+    println!("breaking news! {}", item.summarise());
+} // more verbose way of doing the same thing.
+
+pub fn notify<T: Summary>(item1: &T, item2: &T) // but at least we can choose to use the same types for two parameters.
+
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: std::fmt::Display + Clone,
+    U: std::fmt::Display + Clone + std::fmt::Debug,
+{
+    let x = u;
+    0
+} // it may get messy, so use where
+*/
 
 pub struct NewsArticle {
     pub headline: String,
@@ -17,7 +49,11 @@ pub struct NewsArticle {
 //     }
 // }
 
-impl Summary for NewsArticle {} // use default trait instead of overriding as you'd do above
+impl Summary for NewsArticle {
+    fn summarise_author(&self) -> String {
+        format!("{}", self.author)
+    }
+} // use default trait instead of overriding as you'd do above
 
 pub struct Tweet {
     pub username: String,
@@ -27,6 +63,10 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
+    fn summarise_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+    
     fn summarise(&self) -> String {
         format!("{}: {}", self.username, self.content)
     }
@@ -43,14 +83,51 @@ fn main() {
     println!("one new tweet by {}", t1.summarise());
 
     let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Pittsburgh, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from(
-            "The Pittsburgh Penguins once again are the best \
-             hockey team in the NHL.",
-        ),
+        headline: String::from("The Wall Must Be Built."),
+        location: String::from("Washington, DC, USA"),
+        author: String::from("Fox News Team"),
+        content: String::from("\"We're going to build a wall, and you're paying for it.\"",),
     };
 
-    println!("New article available! {}", article.summarise());
+    println!("new article available: {}", article.summarise());
+
+    notify(&article);
+
+    println!("one new tweet by {}", return_summarisable().summarise());
 }
+
+/*
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+/*
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+} */
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("largest: x, {}", self.x);
+        } else {
+            println!("largest: y, {}", self.y);
+        }
+    }
+}
+
+// implement wildcard
+
+// impl<T: Display> ToString for T {
+// }
+
+fn main() {
+    let x = Pair { x: 9.0, y: 42.0 };
+    
+    x.cmp_display();
+} */
