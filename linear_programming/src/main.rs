afa1d70da888856da61850b07c02c576b217eebe
mashrafi rahman <monomycelium@icloud.com>
2023-02-16 13:03:42 +0800

    add a program to solve linear programming.

---
+++ linear_programming/src/main.rs
@@ -0,0 +1,22 @@
+use std::error::Error;
+use good_lp::{constraint, default_solver, Solution, SolverModel, variables};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    variables! {
+        vars:
+            x <= 500;
+            y <= 500;
+    }
+
+    let solution = vars.maximise(x * 25 + y * 30)
+        .using(default_solver)
+        .with(constraint!(x + y <= 800))
+        .with(constraint!(x >= y / 3))
+        .solve()?;
+
+    println!("number of chocolate chip cookies to bake:\t{}
+number of oatmeal raisin cookies to bake:\t{}
+total profit ($):\t\t\t\t{:2}", solution.value(x), solution.value(y), solution.eval(x * 0.25 + y * 0.30));
+
+    Ok(())
+}
