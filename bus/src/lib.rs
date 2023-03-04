df4718a5004f9eebeee4e56ee351ba0dd5587931
mashrafi rahman <monomycelium@icloud.com>
2023-03-04 23:58:18 +0800

    implement crate `lta` in a command-line tool.

--- bus/src/lib.rs
+++ bus/src/lib.rs
@@ -1,26 +1,68 @@
-use lta::{LTAClient, Client, Bus, BusRequests, models::bus::bus_arrival::BusArrivalResp};
-use chrono::{NaiveTime, Local, Duration};
+use lta::{LTAClient, Client, Bus, BusRequests, models::bus::bus_arrival};
+use chrono::{NaiveDateTime, Local, Duration};
 use clap::Parser;
 use std::error::Error;
 
 #[derive(Parser)]
-pub struct Input {}
+pub struct Input {
+    #[arg(long)]
+    /// api key for lta
+    #[arg(env = "API_KEY")]
+    api_key: String,
+    /// number of timings
+    #[arg(short, long)]
+    #[arg(env = "TIMINGS")]
+    #[arg(default_value = "3")]
+    timings: u8,
+    /// bus stop code
+    #[arg(env = "BUS_STOP_CODE")]
+    // #[arg(default_value = "43389")]
+    bus_stop_code: u32,
+    /// service number
+    #[arg(env = "SERVICE_NO")]
+    // #[arg(default_value = "189")]
+    service_no: String,
+}
 
-pub fn run(input: Input) {
-    
+pub fn run(input: Input) -> Result<(), Box<dyn Error>> {
+    next_bus(input.api_key, input.timings, input.bus_stop_code, input.service_no.as_str())
 }
 
 #[tokio::main]
-async fn get_arrival(api_key: String, bus_stop_code: u32, service_no: &str) -> Result<(), Box<dyn Error>> {
+pub async fn next_bus<'a>(api_key: String, timings: u8, bus_stop_code: u32, service_no: &'a str) -> Result<(), Box<dyn Error>> {
     let client: LTAClient = LTAClient::with_api_key(api_key)?;
-    let mut bus_arrival: BusArrivalResp = Bus::get_arrival(&client, bus_stop_code, service_no).await?;
+    let bus_arrival: bus_arrival::BusArrivalResp = Bus::get_arrival(&client, bus_stop_code, service_no).await?;
 
-    let time: NaiveTime = bus_arrival.services[0].next_bus[0].as_mut().unwrap().est_arrival.time();
+    if let Some(thing) = bus_arrival.services.first() {
+        let now: NaiveDateTime = Local::now().naive_local();
+        let mut count: u8 = 0;
 
-    // let time: NaiveTime = NaiveTime::parse_from_str("2023-03-03T13:18:33+08:00", "%Y-%m-%dT%H:%M:%S+08:00")?;
-    let diff: Duration = time - Local::now().time();
+        for next in thing.next_bus.iter().flatten().map(|next_bus: &bus_arrival::NextBus| next_bus.est_arrival.naive_local()).filter(|time: &NaiveDateTime| time > &now) {
+            let diff: Duration = next - now;
+            println!("{:02}:{:02}", diff.num_minutes(), diff.num_seconds() % 60);
+            count += 1;
 
-    println!("{:02}:{:02}", diff.num_seconds() / 60, diff.num_seconds() % 60);
+            if count == timings {
+                break;
+            }
+        }
 
-    Ok(())
+        if count == 0 {
+            Err("no estimated arrivals found; try again later".into())
+        } else {
+            Ok(())
+        }
+    } else {
+        Err("no data found; try a different bus stop code or service number".into())
+    }
 }
+
+// #[tokio::main]
+// pub async fn next_bus_detailed<'a>(api_key: String, bus_stop_code: u32, service_no: &'a str) -> Result<(), Box<dyn Error>> {
+//     Ok(())
+// }
+
+// #[tokio::main]
+// pub async fn next_bus_detailed_all<'a>(api_key: String, bus_stop_code: u32) -> Result<(), Box<dyn Error>> {
+//     Ok(())
+// }
